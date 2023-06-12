c57cb4f3b4d49ac2a0b7721c00abe94bec5a3145
Tony Lima <tony.lima@globalhitss.com.br>
2023-06-12 17:21:35 -0300

    .

--- index.js
+++ index.js
@@ -44,10 +44,9 @@ app.get(`/:path`, async (req, res) => {
     const db = JSON.parse(
       Buffer.from(result.data.content, 'base64').toString()
     );
-    const start = parseInt(req.query.start) || 0;
-    const limit = parseInt(req.query.limit) || db[req.params.path].apps[0].review.length;
-    console.log("path is: ", req.params.path);
     if (req.params.path === 'reviews') {
+      const start = parseInt(req.query.start) || 0;
+      const limit = parseInt(req.query.limit) || db[req.params.path].apps[0].review.length;
       const data = db[req.params.path].apps[0].review.slice(start, start + limit) || [];
       const formatted = {
         "apps": [
